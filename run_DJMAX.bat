2f6f3f1951fa173861696dae8a2648bf1307740b
Kyuhyun (Joseph) Lee <patmethny1@gmail.com>
2023-12-15 22:45:03 +1100

    Update run_DJMAX.bat

--- run_DJMAX.bat
+++ run_DJMAX.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM (필수) V-아카이브 파일 ""바로가기"" 위치
-set VArchiveLocation="E:\Software\v-archive_v0.4\v-archive.lnk"
+set VArchiveLocation="변경 필요 (대충 .lnk로 끝나는 파일)"
 
 REM 프로그램이 닫힌지 확인하는 주기
 set /A checkTime=10
