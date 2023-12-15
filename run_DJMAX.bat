355edff6003ac878e7459179fd134ec89f9a98c9
Kyuhyun (Joseph) Lee <patmethny1@gmail.com>
2023-12-15 23:35:59 +1100

    Update run_DJMAX.bat

--- run_DJMAX.bat
+++ run_DJMAX.bat
@@ -1,20 +1,32 @@
 @echo off
 chcp 65001 > nul
 
-REM V-아카이브 파일 ""바로가기"" 위치
+REM V-아카이브 파일 ""바로가기"" 위치 ("" 포함)
 set VArchiveLocation="FOLDER/NAME.lnk"
 
-REM 프로그램이 꺼져있는지 확인하는 주기
+REM 프로그램이 꺼져있는지 확인하는 정수 주기 (초)
 set /A checkTime=10
 
-REM 프로그램이 시작할때까지의 시간
+REM 프로그램 확인 주기 시작 시간 | 디맥 실행이 보장되는 정수 시간 (초)
 set /A startupTime=20
 
 REM 디맥 위치 (웬만하면 바꾸지 않아도 될거같음)
 set DJMAXLocation=steam://rungameid/960170
 
 if "%VArchiveLocation:~1,-1%" equ "FOLDER/NAME.lnk" (
-    echo __________ .bat 파일에서 V-아카이브 폴더 위치를 변경해주세요 __________
+    echo __________ V-아카이브 폴더 위치를 변경해주세요 __________
+    pause
+    exit
+)
+
+if %checkTime% lss 1 (
+    echo __________ 프로그램 확인 주기가 너무 빠릅니다 ^(최소 1, 현재 %checkTime%^) __________
+    pause
+    exit
+)
+
+if %startupTime% lss 0 (
+    echo __________ 프로그램 시작시간이 너무 짧습니다 ^(최소 0, 현재 %startupTime%^) __________
     pause
     exit
 )
