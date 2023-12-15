4e41fb03e5ec3646295489fdbe46eed6fb8daa7c
Kyuhyun (Joseph) Lee <patmethny1@gmail.com>
2023-12-15 23:21:58 +1100

    Update run_DJMAX.bat

--- run_DJMAX.bat
+++ run_DJMAX.bat
@@ -1,19 +1,26 @@
 @echo off
+chcp 65001 > nul
 
-REM (필수) V-아카이브 파일 ""바로가기"" 위치
-set VArchiveLocation="폴더/이름.lnk"
+REM V-아카이브 파일 ""바로가기"" 위치
+set VArchiveLocation="FOLDER/NAME.lnk"
 
-REM 프로그램이 닫힌지 확인하는 주기
+REM 프로그램이 꺼져있는지 확인하는 주기
 set /A checkTime=10
 
 REM 프로그램이 시작할때까지의 시간
 set /A startupTime=20
 
-REM 디맥 위치 (웬만하면 바꾸지 않아도 될듯)
+REM 디맥 위치 (웬만하면 바꾸지 않아도 될거같음)
 set DJMAXLocation=steam://rungameid/960170
 
+if "%VArchiveLocation:~1,-1%" equ "FOLDER/NAME.lnk" (
+    echo __________ V-아카이브 폴더 위치를 변경해주세요 __________
+    pause
+    exit
+)
+
 REM 디맥과 V-아카이브 실행
-start /b "" %VArchiveLocation% > NUL 2>&1
+start /b "" "%VArchiveLocation%" > NUL 2>&1
 start /b "" %DJMAXLocation% > NUL 2>&1
 timeout /nobreak /t %startupTime% > NUL
 
