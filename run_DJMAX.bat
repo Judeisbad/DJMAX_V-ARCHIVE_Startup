bb0c4ce70de0549b6cb6bed48b410ebe4bdcd84a
Kyuhyun (Joseph) Lee <patmethny1@gmail.com>
2023-12-15 21:43:26 +1100

    Update run_DJMAX.bat

--- run_DJMAX.bat
+++ run_DJMAX.bat
@@ -1,16 +1,16 @@
 @echo off
 
+REM V-아카이브 파일 ""바로가기"" 위치
+set VArchiveLocation="E:\Software\v-archive_v0.4\v-archive.lnk"
+
 REM 프로그램이 닫힌지 확인하는 주기
 set /A checkTime=10
 
 REM 프로그램이 시작할때까지의 시간
 set /A startupTime=20
 
-REM V-아카이브 파일 ""바로가기"" 위치
-set VArchiveLocation="E:\Software\v-archive_v0.4\v-archive.lnk"
-
-REM 디맥 파일 위치
-set DJMAXLocation="E:\Software\Steam\steamapps\common\DJMAX RESPECT V\DJMAX RESPECT V.exe"
+REM 디맥 위치 (웬만하면 바꾸지 않아도 될거같음)
+set DJMAXLocation=steam://rungameid/960170
 
 REM 디맥과 V-아카이브 실행
 start /b "" %VArchiveLocation% > NUL 2>&1
@@ -31,6 +31,6 @@ if %djmaxClosed% equ 0 (
 goto :wait
 
 :closed
-echo DJMAX RESPECT V is closed. Closing V-ARCHIVE.
+echo 디맥 종료 확인. V-아카이브를 종료합니다.
 taskkill /f /im "v-archive.exe" /t > NUL
-echo V-ARCHIVE is closed.
+echo V-아카이브 종료.
