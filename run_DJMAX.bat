f38b4e3047035af63c969409581e30907512b070
Kyuhyun (Joseph) Lee <patmethny1@gmail.com>
2023-12-16 21:23:32 +1100

    Update run_DJMAX.bat

--- run_DJMAX.bat
+++ run_DJMAX.bat
@@ -13,18 +13,6 @@ if "%VArchiveLocation:~1,-1%" equ "FOLDER/NAME.lnk" (
     exit
 )
 
-if %checkTime% lss 1 (
-    echo __________ 오류: 프로그램 확인 주기가 너무 빠릅니다 ^(최소 1, 현재 %checkTime%^) __________
-    pause
-    exit
-)
-
-if %startupTime% lss 0 (
-    echo __________ 오류: 프로그램 시작시간이 너무 짧습니다 ^(최소 0, 현재 %startupTime%^) __________
-    pause
-    exit
-)
-
 REM 디맥과 V-아카이브 실행
 start /b "" "%VArchiveLocation:~1,-1%" > NUL 2>&1
 start /b /wait "" "%DJMAXLocation:~1,-1%" > NUL 2>&1
