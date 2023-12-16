b93afbbbac260778f70a6dd2fe59b9548b40a6fe
Kyuhyun (Joseph) Lee <patmethny1@gmail.com>
2023-12-16 14:00:36 +1100

    Update run_DJMAX.bat

--- run_DJMAX.bat
+++ run_DJMAX.bat
@@ -2,7 +2,7 @@
 chcp 65001 > nul
 
 REM V-아카이브 파일 ""바로가기"" 위치 ("" 포함)
-set VArchiveLocation="FOLDER/NAME.lnk"
+set VArchiveLocation="E:\Software\v-archive_v0.4\v-archive.lnk"
 
 REM 프로그램이 꺼져있는지 확인하는 주기 (정수, 초)
 set /A checkTime=10
@@ -10,43 +10,28 @@ set /A checkTime=10
 REM 프로그램 확인 주기 시작 시간 / 디맥 실행이 보장되는 시간 (정수, 초)
 set /A startupTime=20
 
-REM 디맥 위치 (웬만하면 바꾸지 않아도 될거같음)
-set DJMAXLocation=steam://rungameid/960170
-
-if "%VArchiveLocation:~1,-1%" equ "FOLDER/NAME.lnk" (
-    echo __________ V-아카이브 폴더 위치를 변경해주세요 __________
-    pause
-    exit
-)
-
-if %checkTime% lss 1 (
-    echo __________ 프로그램 확인 주기가 너무 빠릅니다 ^(최소 1, 현재 %checkTime%^) __________
-    pause
-    exit
-)
-
-if %startupTime% lss 0 (
-    echo __________ 프로그램 시작시간이 너무 짧습니다 ^(최소 0, 현재 %startupTime%^) __________
-    pause
-    exit
-)
+REM 디맥 위치
+set DJMAXLocation="E:\Software\Steam\steamapps\common\DJMAX RESPECT V\DJMAX RESPECT V.exe"
 
 REM 디맥과 V-아카이브 실행
-start /b "" "%VArchiveLocation%" > NUL 2>&1
-start /b "" %DJMAXLocation% > NUL 2>&1
+start /b "" "%VArchiveLocation:~1,-1%" > NUL 2>&1
+start /b "" "%DJMAXLocation:~1,-1%" > NUL 2>&1
+
+REM 디맥 실행까지 기다리기
 timeout /nobreak /t %startupTime% > NUL
 
-REM 디맥 닫힐때까지 기다리기
+echo 디맥 닫힘 여부 확인을 시작합니다
+
+REM 디맥이 닫힐때까지 기다리기
 :wait
-timeout /nobreak /t %checkTime% > NUL
-
 tasklist /fi "imagename eq DJMAX RESPECT V.exe" | find ":" > NUL
-set djmaxClosed=%errorlevel%
+set djmaxRunning=%errorlevel%
 
-if %djmaxClosed% equ 0 (
+if %djmaxRunning% equ 0 (
     goto :closed
 )
 
+timeout /nobreak /t %checkTime% > NUL
 goto :wait
 
 :closed
