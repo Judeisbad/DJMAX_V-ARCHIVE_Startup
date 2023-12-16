a5795e58587055dfc9e01360e460752f125c4f76
Kyuhyun (Joseph) Lee <patmethny1@gmail.com>
2023-12-16 14:07:56 +1100

    Update run_DJMAX.bat

--- run_DJMAX.bat
+++ run_DJMAX.bat
@@ -4,8 +4,6 @@ chcp 65001 > nul
 REM V-아카이브 파일 ""바로가기"" 위치 ("" 포함)
 set VArchiveLocation="FOLDER/NAME.lnk"
 
-REM "E:\Software\v-archive_v0.4\v-archive.lnk"
-
 REM 프로그램이 꺼져있는지 확인하는 주기 (정수, 초)
 set /A checkTime=10
 
@@ -16,19 +14,19 @@ REM 디맥 위치
 set DJMAXLocation="E:\Software\Steam\steamapps\common\DJMAX RESPECT V\DJMAX RESPECT V.exe"
 
 if "%VArchiveLocation:~1,-1%" equ "FOLDER/NAME.lnk" (
-    echo __________ V-아카이브 폴더 위치를 변경해주세요 __________
+    echo __________ 오류: V-아카이브 폴더 위치를 변경해주세요 __________
     pause
     exit
 )
 
 if %checkTime% lss 1 (
-    echo __________ 프로그램 확인 주기가 너무 빠릅니다 ^(최소 1, 현재 %checkTime%^) __________
+    echo __________ 오류: 프로그램 확인 주기가 너무 빠릅니다 ^(최소 1, 현재 %checkTime%^) __________
     pause
     exit
 )
 
 if %startupTime% lss 0 (
-    echo __________ 프로그램 시작시간이 너무 짧습니다 ^(최소 0, 현재 %startupTime%^) __________
+    echo __________ 오류: 프로그램 시작시간이 너무 짧습니다 ^(최소 0, 현재 %startupTime%^) __________
     pause
     exit
 )
