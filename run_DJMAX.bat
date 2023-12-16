5b6c573f098576498fb6f368671174f8aba7209c
Kyuhyun (Joseph) Lee <patmethny1@gmail.com>
2023-12-16 21:43:17 +1100

    Update run_DJMAX.bat

--- run_DJMAX.bat
+++ run_DJMAX.bat
@@ -4,8 +4,8 @@ chcp 65001 > nul
 REM V-아카이브 파일 ""바로가기"" 위치 ("" 포함)
 set VArchiveLocation="FOLDER/NAME.lnk"
 
-REM 디맥 위치
-set DJMAXLocation="E:\Software\Steam\steamapps\common\DJMAX RESPECT V\DJMAX RESPECT V.exe"
+REM 디맥 위치 ("" 포함)
+set DJMAXLocation="C:\Program Files (x86)\Steam\steamapps\common\DJMAX RESPECT V\DJMAX RESPECT V.exe"
 
 if "%VArchiveLocation:~1,-1%" equ "FOLDER/NAME.lnk" (
     echo __________ 오류: V-아카이브 폴더 위치를 변경해주세요 __________
